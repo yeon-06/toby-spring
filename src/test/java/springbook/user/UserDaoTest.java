901306b6d583e4e3bc3baceabf543e926e9716e7
yeon-06 <rnjstldus2@gmail.com>
2022-09-18 00:27:12 +0900

    test: UserDao 테스트 코드 작성

---
+++ src/test/java/springbook/user/UserDaoTest.java
@@ -0,0 +1,37 @@
+package springbook.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserDaoTest {
+
+    private static final UserDao userDao = new UserDao();
+
+    @BeforeEach
+    void delete() {
+        userDao.deleteAll();
+    }
+
+    @AfterEach
+    void delete2() {
+        userDao.deleteAll();
+    }
+
+    @DisplayName(value = "사용자 조회하기")
+    @Test
+    void findById() {
+        // given
+        User user = new User("id", "name", "1234");
+        userDao.add(user);
+
+        // when
+        User actual = userDao.findById(user.getId());
+
+        // then
+        assertThat(actual).isEqualTo(user);
+    }
+}
