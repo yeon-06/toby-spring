85d4977b90912ff32fd8396b3f9520b272b51d09
yeon-06 <rnjstldus2@gmail.com>
2022-09-18 00:29:25 +0900

    refactor: 중복 메서드 추출

--- src/main/java/springbook/user/UserDao.java
+++ src/main/java/springbook/user/UserDao.java
@@ -11,8 +11,7 @@ public class UserDao {
     public void add(User user) {
         String sql = "insert into users(id, name, password) values(?,?,?)";
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:13306/springbook", "spring",
-                "book");
+        try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
             preparedStatement.setString(1, user.getId());
             preparedStatement.setString(2, user.getName());
@@ -27,8 +26,7 @@ public class UserDao {
     public User findById(String id) {
         String sql = "select id, name, password from users where id = ?";
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:13306/springbook", "spring",
-                "book");
+        try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
             preparedStatement.setString(1, id);
 
@@ -49,12 +47,15 @@ public class UserDao {
     public void deleteAll() {
         String sql = "delete from users";
 
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:13306/springbook", "spring",
-                "book");
+        try (Connection connection = getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:13306/springbook", "spring", "book");
     }
 }
