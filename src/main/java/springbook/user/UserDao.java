31b2e6f10d612178d4f4461ac6f720bad9ac3daa
yeon-06 <rnjstldus2@gmail.com>
2022-09-18 00:26:54 +0900

    feat: Users에 대해 create, select, delete 로직 생성

---
+++ src/main/java/springbook/user/UserDao.java
@@ -0,0 +1,60 @@
+package springbook.user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    public void add(User user) {
+        String sql = "insert into users(id, name, password) values(?,?,?)";
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:13306/springbook", "spring",
+                "book");
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setString(1, user.getId());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getPassword());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(String id) {
+        String sql = "select id, name, password from users where id = ?";
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:13306/springbook", "spring",
+                "book");
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.setString(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new User(
+                        resultSet.getString("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("password")
+                );
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteAll() {
+        String sql = "delete from users";
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:13306/springbook", "spring",
+                "book");
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
