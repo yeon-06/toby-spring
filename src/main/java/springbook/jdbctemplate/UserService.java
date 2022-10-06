a0caf5dacbdd88cbf02ae16cfd80dc5a28f95133
yeon-06 <rnjstldus2@gmail.com>
2022-10-06 21:01:36 +0900

    feat: JTA를 이용해 글로벌 트랜잭션으로 변경

--- src/main/java/springbook/jdbctemplate/UserService.java
+++ src/main/java/springbook/jdbctemplate/UserService.java
@@ -1,26 +1,23 @@
 package springbook.jdbctemplate;
 
 import java.util.List;
-import javax.sql.DataSource;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.jta.JtaTransactionManager;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Service
 public class UserService {
 
     private final UserDao userDao;
-    private final DataSource dataSource;
 
-    public UserService(final DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.userDao = new UserDao(dataSource);
+    public UserService(final UserDao userDao) {
+        this.userDao = userDao;
     }
 
     public void upgradeLevels(final List<User> users) {
-        PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
+        PlatformTransactionManager transactionManager = new JtaTransactionManager();
         TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
