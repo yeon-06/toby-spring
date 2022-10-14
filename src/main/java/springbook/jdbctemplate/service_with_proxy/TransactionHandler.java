f9f4263540a0640f0c2a85989e9583f83b40b50f
yeon-06 <rnjstldus2@gmail.com>
2022-10-14 20:45:05 +0900

    feat: InvocationHandler를 이용해 트랜잭션 처리

---
+++ src/main/java/springbook/jdbctemplate/service_with_proxy/TransactionHandler.java
@@ -0,0 +1,36 @@
+package springbook.jdbctemplate.service_with_proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+@Component
+public class TransactionHandler implements InvocationHandler {
+
+    private final Object target;
+    private final PlatformTransactionManager transactionManager;
+
+    public TransactionHandler(final Object target, final PlatformTransactionManager transactionManager) {
+        this.target = target;
+        this.transactionManager = transactionManager;
+    }
+
+    @Override
+    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            Object result = method.invoke(target, args);
+            transactionManager.commit(status);
+            return result;
+
+        } catch (InvocationTargetException e) {
+            transactionManager.rollback(status);
+            throw e.getTargetException();
+        }
+    }
+}
