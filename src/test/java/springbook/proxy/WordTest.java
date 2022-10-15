cba3f3a101ee199da728733ee1e7296c2db2027b
yeon-06 <rnjstldus2@gmail.com>
2022-10-15 16:49:16 +0900

    feat: ProxyFactoryBean을 이용해 다이나믹 프록시 생성

--- src/test/java/springbook/proxy/WordTest.java
+++ src/test/java/springbook/proxy/WordTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.lang.reflect.Proxy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 class WordTest {
 
@@ -28,6 +29,21 @@ class WordTest {
                 new Class[]{Word.class},                    // 다이나믹 프록시가 구현해야 하는 인터페이스 (여러개 가능)
                 new UppercaseHandler(new YeonlogWord())     // InvocationHandler 구현 오브젝트
         );
+
+        // when & then
+        assertThat(proxy.sayHello()).isEqualTo("HELLO YEONLOG!");
+        assertThat(proxy.sayThanks()).isEqualTo("THANK YOU YEONLOG!");
+    }
+
+    @DisplayName(value = "ProxyFactoryBean를 이용해 다이나믹 프록시 생성")
+    @Test
+    void proxyFactoryBean() {
+        // given
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(new YeonlogWord());
+        proxyFactoryBean.addAdvice(new UppercaseAdvice());
+
+        Word proxy = (Word) proxyFactoryBean.getObject();
 
         // when & then
         assertThat(proxy.sayHello()).isEqualTo("HELLO YEONLOG!");
